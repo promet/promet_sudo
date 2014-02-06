6f30e25e1501cd7492d2912fa80bb0811a705234
Greg Palmier <gregpalmier@gmail.com>
2014-02-06 13:37:00 -0600

    init berks'd version.

---
+++ README.md
@@ -0,0 +1,13 @@
+# promet_sudo cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'promet_sudo'
+maintainer       'Promet Solutions, Inc.'
+maintainer_email 'gregpalmier@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures promet_sudo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends		 'apt'
+depends		 'sudo'
